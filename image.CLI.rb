b2fa8ed8c8685ebfc73f6cfc3f76834633e26392
Ben Williams <ben.williams@underdogfantasy.com>
2024-08-15 22:01:59 +0000

    appeasing rubocop

--- image.CLI.rb
+++ image.CLI.rb
@@ -2,53 +2,54 @@ require 'thor'
 require 'exif'
 require 'pathname'
 
+# reads image files and extracts GPS data
 class ImageCLI < Thor
-  desc "hello NAME", "say hello to NAME"
+  desc 'hello NAME', 'say hello to NAME'
   def hello(name)
     puts "Hello #{name}"
   end
 
-  desc "scan [DIRECTORY]", "scan the DIRECTORY"
-  def scan (directory = ".")
+  desc 'scan [DIRECTORY]', 'scan the DIRECTORY'
+  def scan(directory = '.')
     puts "Scanning #{directory}"
     process_directory(directory)
   end
 
-  private 
-def process_directory(directory)
-  Dir.foreach(directory)  do |file|
-    next if file.start_with?('.')
-    next if file.start_with?('_')
-    full_path = File.join(directory, file)
-    
-    if (Dir.exist?(full_path))
-      puts "Directory: #{full_path}"
-      process_directory(full_path)      
-    elsif file.end_with?("jpg")
-      file_pathname = Pathname.new(full_path)
-      process_file file_pathname
+  private
+
+  def process_directory(directory)
+    Dir.foreach(directory) do |file|
+      full_path = File.join(directory, file)
+
+      if (Dir.exist?(full_path) unless file.start_with?('.'))
+        puts "Directory: #{full_path}"
+        process_directory(full_path)
+      elsif file.end_with?('jpg')
+        file_pathname = Pathname.new(full_path)
+        process_file file_pathname
+      end
     end
   end
-end
 
   def process_file(filepath)
     # puts filepath
     absolute_path = filepath.expand_path
     # puts "Absolute Path: #{absolute_path}"
+    location = get_geo_data(absolute_path)
+    puts "#{absolute_path} (#{location})"
+  end
+
+  def get_geo_data(absolute_path)
     f = File.open(absolute_path)
-    begin
-      data = Exif::Data.new(f) # load from file
-    rescue Exif::NotReadable
-      puts "#{absolute_path} Not a readable file"
-      return
+    data = Exif::Data.new(f) # load from file
+
+    if data.gps_latitude.nil? || data.gps_longitude.nil?
+      'No GPS data found'
+    else
+      stringify_geo data.gps_latitude, data.gps_latitude_ref
     end
-    if (data.gps_latitude.nil? || data.gps_longitude.nil?)
-      puts "#{absolute_path} No GPS data found"
-      return
-    end
-    lat = stringify_geo data.gps_latitude, data.gps_latitude_ref
-    long = stringify_geo data.gps_longitude, data.gps_longitude_ref
-    puts "#{absolute_path} (#{lat})  (#{long})"
+  rescue Exif::NotReadable
+    'Not a readable file'
   end
 
   def stringify_geo(position, geo_ref)
@@ -58,7 +59,5 @@ end
     "#{degrees}° #{minutes}' #{seconds}\" #{geo_ref}"
   end
 end
-
-
 
 ImageCLI.start(ARGV)
