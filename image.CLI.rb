cb0be596b50cb46ae9b75282c9ad3d9efe77ecfc
Ben Williams <ben.williams@underdogfantasy.com>
2024-08-15 21:43:17 +0000

    prettify output

--- image.CLI.rb
+++ image.CLI.rb
@@ -42,8 +42,23 @@ end
       puts "#{absolute_path} Not a readable file"
       return
     end
-    puts "#{absolute_path} #{data.gps_longitude}"
+    if (data.gps_latitude.nil? || data.gps_longitude.nil?)
+      puts "#{absolute_path} No GPS data found"
+      return
+    end
+    lat = stringify_geo data.gps_latitude, data.gps_latitude_ref
+    long = stringify_geo data.gps_longitude, data.gps_longitude_ref
+    puts "#{absolute_path} (#{lat})  (#{long})"
+  end
+
+  def stringify_geo(position, geo_ref)
+    degrees = position[0].to_f
+    minutes = position[1].to_f
+    seconds = position[2].to_f
+    "#{degrees}° #{minutes}' #{seconds}\" #{geo_ref}"
   end
 end
+
+
 
 ImageCLI.start(ARGV)
